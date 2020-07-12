f0cc23a0184e9413c7004c3eb26bd860b0ecdd51
C.y <yangruihan@vip.qq.com>
2020-07-12 11:08:29 +0800

    优化项目结构，将主循环放到lib中

--- libray/src/ray.c
+++ libray/src/ray.c
@@ -1,9 +1,39 @@
 #include "ray.h"
 
+// Lua
+#include <lua.h>
+#include <lualib.h>
+#include <lauxlib.h>
+
 #include "timer/timer_wrap.h"
 
-int ray_lua_init(lua_State* L)
+static int ray_lua_init(lua_State* L)
 {
     lua_open_ray_timer(L);
+    return 0;
+}
+
+int ray_start()
+{
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+
+    lua_newtable(L);
+    lua_setglobal(L, "ray");
+
+    ray_lua_init(L);
+
+    lua_getglobal(L, "require");
+    lua_pushstring(L, "scripts.ray.boot");
+    lua_call(L, 1, 1);
+
+    lua_newthread(L);
+    lua_pushvalue(L, -2);
+    int stackpos = lua_gettop(L);
+    while (lua_resume(L, NULL, 0) == LUA_YIELD)
+        lua_pop(L, lua_gettop(L) - stackpos);
+
+    lua_close(L);
+
     return 0;
 }
--- libray/src/ray.h
+++ libray/src/ray.h
@@ -1,9 +1,8 @@
 #ifndef LIBRAY_RAY_H_
 #define LIBRAY_RAY_H_
 
-#include "lua.h"
 #include "common/config.h"
 
-LIBRAY_API int ray_lua_init(lua_State* L);
+LIBRAY_API int ray_start();
 
 #endif
